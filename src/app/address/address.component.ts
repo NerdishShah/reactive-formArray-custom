e01613e868247773938fd6acd04c145ab89967ff
sha07geek <sha07geek.gmail.com>
2020-02-11 23:48:30 +0400

    initial commit

---
+++ src/app/address/address.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+@Component({
+  selector: "address",
+  templateUrl: "./address.component.html",
+  styleUrls: ["./address.component.css"]
+})
+export class AddressComponent implements OnInit {
+  @Input("group")
+  public adressForm: FormGroup;
+  constructor() {}
+
+  ngOnInit() {}
+}
